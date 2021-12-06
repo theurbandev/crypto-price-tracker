6cb79db789416f99e3c11619ed4f3ffab8479efc
Matthew Joseph <matthew.j.joe@gmail.com>
2021-12-06 15:44:05 -0500

    Coin list api call

--- crypto-price-tracker/src/app/components/dashboard/dashboard.component.ts
+++ crypto-price-tracker/src/app/components/dashboard/dashboard.component.ts
@@ -8,26 +8,58 @@ import axios from 'axios';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
-
   ngOnInit() {
+    this.coinListApiCall();
   }
   
+  isDataAvailable: boolean = false;
 
-  testApiCall() {
+  coinsList:any = {
+      coins: []
+  }
+
+  coinListApiCall() 
+  {
     console.log("Starting api call...")
     axios({
       method: 'get',
-      url: 'https://api.coingecko.com/api/v3/coins/bitcoin',
+      url: 'https://api.coingecko.com/api/v3/coins/list',
       headers: {
-        'id': 'bitcoin',
         'Access-Control-Allow-Origin': "*",
         'Content-Type': 'application/json'
       }
     })  
     .then( (response: any) => {
+      for(let i=0; i < 20; i++){
+        this.coinsList.coins.push(response.data[i].id);
+      }
+
+      this.isDataAvailable = true;
+      this.priceListApiCall();
+      //console.log(response);
+    });
+  }
+
+  priceListApiCall()
+  {
+    console.log("Starting api call...")
+    axios({
+      method: 'get',
+      url: `https://api.coingecko.com/api/v3/simple/price?ids=${this.coinsList.coins}&vs_currencies=usd`,
+      headers: {
+        'Access-Control-Allow-Origin': "*",
+        'Content-Type': 'application/json'
+      }
+    })  
+    .then( (response: any) => {
+
       console.log(response);
     });
   }
 
+  parseCoinsList()
+  {
+    
+  }
+  
 }
